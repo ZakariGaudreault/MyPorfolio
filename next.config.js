6e97206095e44550ce7c7f02bc7e26ff0e705cfb
ZakariGaudreault <zakarigaudreault@hotmail.com>
2025-10-17 01:46:40 -0400

    changed my configs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: "export",
   images: { unoptimized: true },
+  basePath: isProd ? "/MyPorfolio" : "",
+  assetPrefix: isProd ? "/MyPorfolio/" : "",
 };
 
 module.exports = nextConfig;
